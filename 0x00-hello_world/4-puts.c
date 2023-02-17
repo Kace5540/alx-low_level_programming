be7823958277b59059816757290427178cd4dd10
Kemo <kemosillah@yahoo.com>
2023-02-17 05:14:24 -0800

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio>
-/** this is my first c program */
+/**
+ * main - a program that prints line with puts function
+ * Return: 0 (Success)
+  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
