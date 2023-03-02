b826698d3e193759f509881af1f3fb461713e7ab
Kemo <kemosillah@yahoo.com>
2023-03-02 04:34:32 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - print sign
+ * @n: the number to be checked
+ * Return: 1 for positive 
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+}
+	else if (n < 0)
+	{_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
