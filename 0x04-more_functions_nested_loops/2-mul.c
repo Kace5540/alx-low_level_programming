0812c7b118786bcb1601a8dba5e9620db87acbd3
Kemo <kemosillah@yahoo.com>
2023-03-04 05:52:52 -0800

    l

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul : multiples two numbers
+ * @x: the first num
+ * @y: the second num
+ * Return: a ans for upper case and 0 for others
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
