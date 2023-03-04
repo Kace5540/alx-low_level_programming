9d73e5b0eb9af7bc513c90101b5cac5e3f739666
Kemo <kemosillah@yahoo.com>
2023-03-04 12:04:42 -0800

    l

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,6 +9,7 @@
 void print_square(int size)
 {
 	int x, y;
+	i
 	if (size <= 0)
 	{
 		_putchar('\n');
@@ -17,7 +18,7 @@ void print_square(int size)
 	{
 		for (x = 0; x < size; x++)
 		{
-			for (y = 0; x < size; y++)
+			for (y = 0; y < size; y++)
 			{
 				_putchar(35);
 			}
