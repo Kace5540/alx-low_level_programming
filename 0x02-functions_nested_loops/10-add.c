5096c32e1a2c27ab822c633639a06328971a35f6
Kemo <kemosillah@yahoo.com>
2023-03-04 11:56:45 -0800

    l

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * 10-add.c - adds two numbers and return the result as an int
+ * add - adds two numbers and return the result as an int
  * @x: the first value
  * @y: the second value
  * Return: 0
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,13 +4,13 @@
  * @c: the number to be computed
  * Return: abs
  */
-
 int _abs(int c)
 {
 	if (c < 0)
 	{
 		int abs_val;
-		abs_val =c * -1;
+
+		abs_val = c * -1;
 		return (abs_val);
 	}
 	return (c);
