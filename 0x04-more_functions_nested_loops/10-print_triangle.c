f4e0920ef79d032d3523cf7c6ff385aad947e999
Kemo <kemosillah@yahoo.com>
2023-03-04 13:12:26 -0800

    l

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints n squares according n numbewer
+ * @size: the number of squares
+ * Return: empty
+ */
+
+void print_triangle(int size)
+{
+        int x, y;
+
+        if (size <= 0)
+        {
+                _putchar('\n');
+        }
+        else
+	{
+	for (x = 0; x < size; x++)
+	{
+	for (y = size - x; y < 1; y--)
+	{
+		_putchar(32);
+	}
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+}
