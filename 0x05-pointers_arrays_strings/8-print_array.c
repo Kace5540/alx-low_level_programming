08b356a210633227a866a8de456262f4b9f2a37b
Kemo <kemosillah@yahoo.com>
2023-03-05 11:05:03 -0800

    l

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - the function prints
+ * @a: the value 
+ * @n: the value
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n -1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+			printf("\n");
+}
